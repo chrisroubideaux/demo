c6cdba4724d58d9955f03a2bd50e7a1e97f7b15d
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2022-08-11 01:32:31 -0500

    Update fetchBooks.js

--- src/api/fetchBooks.js
+++ src/api/fetchBooks.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL =
-  'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=A8U74mXf0N8lMVrtz0TdUQkG4iHhjE9o';
+  'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=';
 
 export const fetchBooks = async (query) => {
   const { data } = await axios.get(URL, {
